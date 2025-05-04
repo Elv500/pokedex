25fa6f9f074298527fc4d3c4c7acf3c5a4ddf158
Elv500 <elvis.alvarez.2001@gmail.com>
2025-05-04 19:55:23 -0400

    Asegurando ordenamiento - Optimizando tiempo de carga - Skeleton

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const lista = document.getElementById('lista');
 
-function showPokemons(cantidad) {
-    for(let i=1; i<=cantidad; i++){
-        pokeapi(i);
+async function showPokemons(cantidad) {
+    
+    showSkeletons(cantidad);
+    
+    console.time('Measuring time');
+    const promises = [];
+    
+    for (let i = 1; i <= cantidad; i++) {
+        promises.push(pokeapi(i));
     }
+    
+    const pokemons = await Promise.all(promises);
+
+    lista.innerHTML = '';
+
+    pokemons.sort((a, b) => a.id - b.id);
+    pokemons.forEach(createPokemon);
     document.body.append(lista);
+    console.timeEnd('Measuring time');
 }
 
-async function pokeapi(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+async function pokeapi(id){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`, {cache: "no-store"});
     const data = await response.json();
-    createPokemon(data);
+    return data;
 }
 
-function createPokemon(pokemon) {
+function createPokemon(pokemon){
+
     const imagen = document.createElement('img');
     imagen.src = pokemon.sprites.front_default;
 
     const number = document.createElement('p');
-    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
 
     const name = document.createElement('div');
     name.textContent = pokemon.name;
@@ -31,4 +46,23 @@ function createPokemon(pokemon) {
     lista.append(element);
 }
 
-showPokemons(90);
+showPokemons(80);
+
+function showSkeletons(count) {
+    for (let i = 0; i < count; i++) {
+        const skeleton = document.createElement('li');
+        skeleton.classList.add('skeleton-card');
+
+        const img = document.createElement('div');
+        img.classList.add('skeleton', 'skeleton-img');
+
+        const line1 = document.createElement('div');
+        line1.classList.add('skeleton', 'skeleton-text');
+
+        const line2 = document.createElement('div');
+        line2.classList.add('skeleton', 'skeleton-text');
+
+        skeleton.append(line1, img, line2);
+        lista.append(skeleton);
+    }
+}
